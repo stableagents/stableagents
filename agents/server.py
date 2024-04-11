ab6ff14dc360c029ba7d474b1539a1384a532df8
Jordan Plows <jordan@lightapi.com>
2024-04-11 16:33:44 -0700

    dev: janitor commit

--- agents/server.py
+++ agents/server.py
@@ -1,11 +1,18 @@
 from fastapi import fastapi
 
 import uvicorn
+import json 
 
 
 def server(agents, host="0.0.0.0", port=8000):
     app = FastAPI()
+    router = APIRouter()
 
     @app.post("/test/agents")
     async def action():
         return {"message": "test"}
+    
+    @app.
+
+
+uvicorn.run(app, host=host, port=port)
