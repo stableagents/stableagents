a5d05a3577ecc05493006100c4d56f9166ca910e
Jordan Plows <jordan@lightapi.com>
2024-04-13 16:19:36 -0700

    dev: janitor commit

--- agents/interface/interface.py
+++ agents/interface/interface.py
@@ -6,4 +6,5 @@ class interface():
     pass
 
 
+
     
---
+++ agents/main/llms/main.py
@@ -0,0 +1,7 @@
+## Here you will attach or call your own model.. you can bring your own model or pay for SLAM-1 which was designed to work with Stable Agents
+
+
+import json 
+
+class llm_switcher():
+    pass
