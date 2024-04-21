402c6d2fa4a18a357c8d0bfa7bbf9ac59a5e5117
Jordan Plows <jordan@lightapi.com>
2024-04-21 14:40:53 -0700

    dev: janitor commit

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,3 @@
 from main import * 
 
-agents = StableAgents()
+
--- agents/main/main_server.py
+++
@@ -1,6 +0,0 @@
-import json
-import typing Generator 
-
-import uvicorn 
-from fastapi import Body, FastAPI, Request, Response, WebSocket
-from fastapi.response import PlainTextResponse, StreamingResponse 
