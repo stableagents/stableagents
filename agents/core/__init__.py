7d87da6926b7f395911159ba157247f65eebe142
Jordan Plows <jordan@lightapi.com>
2025-01-31 01:13:04 -0800

    dev: janitor commit

--- agents/core/__init__.py
+++ agents/core/__init__.py
@@ -1,18 +1,18 @@
-# This is a single-line comment
-# You can comment multiple lines
-# by adding # at the start of each line
-# like this
+from agents.walkietalkie.hallucination_predictor import create_hallucination_pipeline, process_text
+from agents.walkietalkie.main import create_demo_router
 
-'''
-This is a multi-line comment
-Everything between the triple quotes
-will be commented out
-You can write as many lines as you want
-'''
+def initialize_walkie_talkie():
+    """Initialize and configure the walkie talkie components."""
+    # Set up hallucination detection pipeline
+    detector, validator, router = create_hallucination_pipeline()
+    
+    # Create response routing system
+    response_router = create_demo_router()
+    
+    return {
+        'hallucination_detector': detector,
+        'hallucination_validator': validator, 
+        'hallucination_router': router,
+        'response_router': response_router
+    }
 
-# Or using double quotes
-"""
-This is also a multi-line comment
-Everything between the triple quotes
-will be commented out
-"""
