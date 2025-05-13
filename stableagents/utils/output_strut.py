7edca83c6fdbe45a5ef24036f974509b05301665
Jordan Plows <jordan@lightapi.com>
2025-05-12 21:17:27 -0700

    dev: janitor commit

--- stableagents/utils/output_strut.py
+++ stableagents/utils/output_strut.py
@@ -5,19 +5,5 @@ import json
 from pydantic import BaseModel
 from typing import Any, Callable
 
-class OutputParserError(Exception):
-    """
-    Exception raised when the output parser fails to parse the output
-    """
 
-    def __init__(self, message, output=None):
-        self.message = message
-        self.output = output
-        super().__init__(self.message)
-    
-    def __str__(self):
-        if self.output:
-           return f"{self.message}\nProblematic output: {self.output}"
-        return self.message
-    
  
