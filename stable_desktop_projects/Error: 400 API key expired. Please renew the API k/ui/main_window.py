c459b4fe08b2cb68c6a1f5c7b873d3c2e40d2091
Jordan Plows <jordan@lightapi.com>
2025-06-26 11:32:58 -0700

    dev: janitor commit

--- stable_desktop_projects/Error: 400 API key expired. Please renew the API k/ui/main_window.py
+++
@@ -1,9 +0,0 @@
-Error: 400 API key expired. Please renew the API key. [reason: "API_KEY_INVALID"
-domain: "googleapis.com"
-metadata {
-  key: "service"
-  value: "generativelanguage.googleapis.com"
-}
-, locale: "en-US"
-message: "API key expired. Please renew the API key."
-]
